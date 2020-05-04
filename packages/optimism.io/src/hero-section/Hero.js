522810d162f01d3dbfc6a4602da8b622866fc50b
Jinglan Wang <jwang5@wellesley.edu>
2020-05-03 20:50:12 -0400

    add styling for hero

---
+++ packages/optimism.io/src/hero-section/Hero.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import styles from './Hero.module.css';
+import heroImage from './hero.svg';
+
+class Hero extends Component {
+  render() {
+    return (
+    	<div  class={styles.hero}>
+	    	<div>
+
+	    		<div class={styles.welcomeMessage}>
+	    			<h1>The New Scalability Stack for Ethereum</h1>
+	    			<p>The Optimistic Virtual Machine (OVM) is the 
+	    			best software platform for running your Ethereum dApp.</p>
+	    		</div>
+	    		<div class={styles.transitionMessage}>
+	    			<h2>SYNTHETIX.EXCHANGE</h2>
+	    			<h2>CASE STUDY</h2>
+	    			<div>
+	    				<a href={"#Demo"} class={styles.roundButton}>⇩</a>
+	    			</div>
+	    		</div>
+	    		<div class={styles.bg}></div>
+	    	</div>
+
+    	</div>
+    )
+  }
+}
+
+export default Hero;
