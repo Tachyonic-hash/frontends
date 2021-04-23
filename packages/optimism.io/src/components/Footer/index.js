42e9d9c7c6f2e2bb2ff55c238abeae5d8bca8e07
platocrat <jack.abe.winfield@gmail.com>
2021-04-22 18:44:43 -0700

    fix: copyright year auto-update

--- packages/optimism.io/src/components/Footer/index.js
+++ packages/optimism.io/src/components/Footer/index.js
@@ -5,63 +5,65 @@ import { Link as ReactLink } from 'react-router-dom';
 import { navCategories } from '../../constants';
 import SocialLinks from '../SocialLinks';
 class Footer extends Component {
-  render() {
+  render () {
+    let currentYear = new Date().getFullYear()
+
     return (
-      <Box as="footer" backgroundColor="black" mt={32}>
-        <Container pt={8} maxW="1200px">
+      <Box as="footer" backgroundColor="black" mt={ 32 }>
+        <Container pt={ 8 } maxW="1200px">
           <SimpleGrid
-            gridTemplateColumns={[
+            gridTemplateColumns={ [
               'repeat(2, 1fr)',
               'repeat(2, minmax(max-content,100%))',
               null,
               'repeat(4, minmax(max-content,100%))',
-            ]}
-            spacingX={[4, 8, 16]}
-            spacingY={8}
+            ] }
+            spacingX={ [ 4, 8, 16 ] }
+            spacingY={ 8 }
           >
-            {navCategories.map((col, idx) => (
-              <VStack alignItems="flex-start" key={idx}>
+            { navCategories.map((col, idx) => (
+              <VStack alignItems="flex-start" key={ idx }>
                 <Heading as="h3" color="white" fontSize="xl">
-                  {col.heading}
+                  { col.heading }
                 </Heading>
                 {col.items.map((item) =>
                   item.internal ? (
                     <Link
-                      key={item.url}
-                      as={ReactLink}
-                      to={item.url}
-                      fontSize={['md', 'lg']}
-                      mb={1}
+                      key={ item.url }
+                      as={ ReactLink }
+                      to={ item.url }
+                      fontSize={ [ 'md', 'lg' ] }
+                      mb={ 1 }
                       color="white"
                     >
-                      {item.name}
+                      {item.name }
                     </Link>
                   ) : (
                     <Link
-                      key={item.url}
-                      href={item.url}
-                      fontSize={['md', 'lg']}
-                      mb={1}
+                      key={ item.url }
+                      href={ item.url }
+                      fontSize={ [ 'md', 'lg' ] }
+                      mb={ 1 }
                       color="white"
                       target="_blank"
                       rel="noopenner noreferrer"
                     >
-                      {item.name}
+                      {item.name }
                     </Link>
                   )
-                )}
+                ) }
               </VStack>
-            ))}
+            )) }
           </SimpleGrid>
           <Box
             maxW="containerLg"
-            py={12}
+            py={ 12 }
             color="white"
             d="flex"
             justifyContent="space-between"
           >
-            <Box as="span" opacity={0.5}>
-              Copybyte Optimism PBC 2020. All flights reserved.
+            <Box as="span" opacity={ 0.5 }>
+              Copybyte Optimism PBC 2020 - { currentYear }. All flights reserved.
             </Box>
             <SocialLinks />
           </Box>
